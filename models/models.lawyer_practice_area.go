c1f93094d41cfedb64dbbca0331aed424081d35e
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2022-02-17 05:23:41 +0530

    changed file name

--- models/models.lawyer_practice_area.go
+++ models/models.lawyer_practice_area.go
@@ -6,7 +6,7 @@ type LawyerPracticeArea struct {
 	ID             uint      `gorm:"primaryKey" json:"id"`
 	LawyerID       uint      `gorm:"not null" json:"lawyer_id"`
 	PracticeAreaID uint      `gorm:"not null" json:"practice_area_id"`
-	Charge         uint      `gorm:"not null" json:"charge"`
+	Charge         int       `gorm:"" json:"charge"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
 }
