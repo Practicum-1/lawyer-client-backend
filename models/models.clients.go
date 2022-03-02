21526f16687d82e4a9657825620efaef1b478995
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2022-03-03 04:30:08 +0530

    added virtual fields

--- models/models.clients.go
+++ models/models.clients.go
@@ -8,15 +8,15 @@ import (
 )
 
 type Client struct {
-	ID        uint   `gorm:"primaryKey" json:"id"`
-	FirstName string `gorm:"not null" json:"first_name"`
-	LastName  string `gorm:"" json:"last_name"`
-	// FullName  string   `gorm:"->;type:GENERATED ALWAYS AS (concat(firstname,' ',lastname));default:(-);"`
-	Location         string                 `gorm:"not null" json:"location"`
+	ID               uint                   `gorm:"primaryKey" json:"id"`
+	FirstName        string                 `gorm:"type:varchar(100) not null" json:"first_name"`
+	LastName         string                 `gorm:"type:varchar(100) not null" json:"last_name"`
+	FullName         string                 `gorm:"->;type:text GENERATED ALWAYS AS (concat(first_name,' ',last_name)) VIRTUAL;" json:"full_name"`
+	Location         string                 `gorm:"type:text" json:"location"`
 	Email            string                 `gorm:"unique;not null" json:"email"`
-	Phone            string                 `gorm:"not null" json:"phone"`
-	Password         string                 `gorm:"not null" json:"password"`
-	ImageURL         string                 `gorm:"" json:"image_url"`
+	Phone            string                 `gorm:"type:varchar(100);not null" json:"phone"`
+	Password         string                 `gorm:"type:varchar(100);not null" json:"password"`
+	ImageURL         string                 `gorm:"type:text" json:"image_url"`
 	Reviews          []Review               `gorm:"foreignKey:ClientID" json:"reviews"`
 	Requests         []Request              `gorm:"foreignKey:ClientID" json:"requests"`
 	ConnectedLawyers []LawyerClientRelation `gorm:"foreignKey:ClientID" json:"connected_lawyers"`
--- models/models.lawyer.go
+++ models/models.lawyer.go
@@ -9,14 +9,15 @@ import (
 )
 
 type Lawyer struct {
-	ID        uint   `gorm:"primaryKey" json:"id"`
-	FirstName string `gorm:"not null" json:"first_name"`
-	LastName  string `gorm:"" json:"last_name"`
-	// FullName      string         `gorm:"->;type:GENERATED ALWAYS AS (concat(first_name,' ',last_name));default:(-);"`
-	Location         string                 `gorm:"not null" json:"location"`
+	ID               uint                   `gorm:"primaryKey" json:"id"`
+	FirstName        string                 `gorm:"type:varchar(100) not null" json:"first_name"`
+	LastName         string                 `gorm:"type:varchar(100) not null" json:"last_name"`
+	FullName         string                 `gorm:"->;type:text GENERATED ALWAYS AS (concat(first_name,' ',last_name)) VIRTUAL;" json:"full_name"`
+	Location         string                 `gorm:"type:text" json:"location"`
 	Email            string                 `gorm:"unique;not null" json:"email"`
-	Password         string                 `gorm:"not null" json:"password"`
-	ImageURL         string                 `gorm:"" json:"image_url"`
+	Phone            string                 `gorm:"type:varchar(100);not null" json:"phone"`
+	Password         string                 `gorm:"type:varchar(100);not null" json:"password"`
+	ImageURL         string                 `gorm:"type:text" json:"image_url"`
 	Education        string                 `gorm:"not null" json:"education"`
 	Experience       uint                   `gorm:"not null" json:"experience"`
 	Gender           string                 `gorm:"not null" json:"gender"`
@@ -24,7 +25,6 @@ type Lawyer struct {
 	Courts           []Court                `gorm:"many2many:lawyer_courts" json:"courts"`
 	Languages        []Language             `gorm:"many2many:lawyer_languages" json:"languages"`
 	PracticeAreas    []LawyerPracticeArea   `gorm:"foreignKey:LawyerID" json:"practice_areas"`
-	Phone            string                 `gorm:"not null" json:"phone"`
 	PendingRequests  []Request              `gorm:"foreignKey:LawyerID;check: status === 'pending'" json:"pending_requests"` //just for testing
 	Requests         []Request              `gorm:"foreignKey:LawyerID" json:"requests"`
 	ConnectedClients []LawyerClientRelation `gorm:"foreignKey:LawyerID" json:"connected_lawyers"`
